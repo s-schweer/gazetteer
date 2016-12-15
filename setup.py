ec8e6fd32bcc3a85bb085faf6c523ea13b3a0cb5
Stefan Schweer <s.schweer@douglas.de>
2016-12-15 11:53:37 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gazetteer',
+    version='',
+    packages=['src.gazetteer'],
+    url='',
+    license='',
+    author='stefan',
+    author_email='',
+    description=''
+)
